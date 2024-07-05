4c472f6b0bd3b5a3608aee9d970830ef88a8ea3a
abhishekmurthy72 <abhishekmurthy72@gmail.com>
2024-07-05 19:50:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 resource "aws_instance" "instance1" {
   ami = var.ami_value
   instance_type = var.instance_type_value
 }
-
 resource "aws_s3_bucket" "abhishekmurthy72_bucket" {
     bucket = "abhishekmurthy-bucket-xyz"
     force_destroy = true
