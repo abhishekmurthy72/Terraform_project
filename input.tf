efd6e94806375dc1a13719e25e99a2cb7ba79db7
abhipersonalorg <abhishekmurthy72@gmail.com>
2025-04-01 22:34:02 +0530

    Update input.tf

--- input.tf
+++ input.tf
@@ -9,9 +9,3 @@ variable "instance_type_value" {
   type = string
   default = "t2.micro"
 }
-
-#variable "instance_type_value" {
- # description = "Default Linux of AMI"
-  #type = string
-  #default = "t2.micro"
-#}
