c96c594b085bd02a9fde45795a5bb21257cabe1b
abhishekmurthy72 <abhishekmurthy72@gmail.com>
2026-04-09 16:43:33 +0530

    testing main2.tf need to be removed later

---
+++ main2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_instance" "instance1" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+    # Ensure associate_public_ip_address is defined explicitly
+  associate_public_ip_address = false    # or false, based on your requirement
+}
+
+resource "aws_s3_bucket" "abhishekmurthy72_bucket" {
+    bucket = "abhishekmurthy-bucket-xyz"
+    force_destroy = true
+}
+# nothing just test...
